44e512b3bd49fdc3d20b7b77bebfd23b5e38d296
Ravindra Bansode <47310931+rbansode29@users.noreply.github.com>
2019-09-04 14:17:48 +0900

    Update and rename ProposeWithChololate.java to ProposeWithChocolate.java

---
+++ ProposeWithChocolate.java
@@ -0,0 +1,8 @@
+/**
+class ProposeWithChocolate was used to implement Propose interface with dark chocolate strategies.
+*/
+class ProposeWithChocolate implements Propose{
+    public void doPropose(){
+        System.out.println("Dear.... this Dark chocolate is for you !!!!!!");
+    }
+}
--- ProposeWithChololate.java
+++
@@ -1,8 +0,0 @@
-/**
-class ProposeWithChololate was used to implement Propose interface with dark chocolate strategies.
-*/
-class ProposeWithChololate implements Propose{
-    public void doPropose(){
-        System.out.println("Dear.... this Dark chocolate is for you !!!!!!");
-    }
-}
