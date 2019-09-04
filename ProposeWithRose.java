f7a30641280300302ee9cba18d9a284c4962eff3
Ravindra Bansode <47310931+rbansode29@users.noreply.github.com>
2019-09-04 14:15:15 +0900

    Create ProposeWithRose.java
    class - ProposeWithRose is used to implement the Propose interface with Rose Strategies.

---
+++ ProposeWithRose.java
@@ -0,0 +1,8 @@
+/**
+class - ProposeWithRose is used to implement the Propose interface with Rose Strategies.
+*/
+class ProposeWithRose implements Propose{
+    public void doPropose(){
+        System.out.println("Dear.... this red rose is for you !!!!!!");
+    }
+}
