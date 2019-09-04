0bc405dc08cfe37243cf94f0a7aaa411e60f7722
Ravindra Bansode <47310931+rbansode29@users.noreply.github.com>
2019-09-04 14:20:00 +0900

    Create ProposeWithRing.java
    class ProposeWithRing was used to implement Propose interface with Ring strategies.

---
+++ ProposeWithRing.java
@@ -0,0 +1,9 @@
+/**
+class ProposeWithRing was used to implement Propose interface with Ring strategies.
+*/
+
+class ProposeWithRing implements Propose{
+    public void doPropose(){
+        System.out.println("Dear.... this Ring is for you !!!!!!");
+    }
+}
