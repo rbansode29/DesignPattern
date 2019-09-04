00875a57022fd0ccf23ecfbb00325ddbd6acfe7d
Ravindra Bansode <47310931+rbansode29@users.noreply.github.com>
2019-09-04 14:17:01 +0900

    Create ProposeWithChololate.java
    class ProposeWithChololate was used to implement Propose interface with dark chocolate strategies.

---
+++ ProposeWithChololate.java
@@ -0,0 +1,8 @@
+/**
+class ProposeWithChololate was used to implement Propose interface with dark chocolate strategies.
+*/
+class ProposeWithChololate implements Propose{
+    public void doPropose(){
+        System.out.println("Dear.... this Dark chocolate is for you !!!!!!");
+    }
+}
